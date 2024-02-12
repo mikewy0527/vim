f336143c579eff0d4567ba4b14cdce7aec2258a4
skywind3000 <skywind3000@163.com>
2024-02-12 11:34:03 +0800

    commit gitcommit.py

--- lib/gptcommit.py
+++ lib/gptcommit.py
@@ -73,6 +73,41 @@ def load_ini(filename, encoding = None):
 
 
 #----------------------------------------------------------------------
+# execute
+#----------------------------------------------------------------------
+def execute(args):
+    import subprocess
+    p = subprocess.Popen(args, shell = True,
+                         stdin = subprocess.PIPE, 
+                         stdout = subprocess.PIPE,
+                         stderr = subprocess.STDOUT)
+    stdin, stdouterr = (p.stdin, p.stdout)
+    stdin.close()
+    content = stdouterr.read()
+    stdouterr.close()
+    p.wait()
+    guess = [sys.getdefaultencoding(), 'utf-8']
+    import locale
+    guess.append(locale.getpreferredencoding())
+    for name in guess + ['gbk', 'ascii', 'latin1']:
+        try:
+            text = content.decode(name)
+            return text
+        except:
+            pass
+    return content.decode(sys.stdout.encoding, 'ignore')
+
+
+#----------------------------------------------------------------------
+# call git
+#----------------------------------------------------------------------
+def CallGit(*args):
+    lines = execute(['git'] + list(args))
+    content = [line.rstrip('\r\n\t ') for line in lines.split('\n')]
+    return '\n'.join(content)
+
+
+#----------------------------------------------------------------------
 # lazy request
 #----------------------------------------------------------------------
 LAZY_OPTION = None
@@ -98,6 +133,44 @@ def chatgpt_lazy(messages):
 
 
 #----------------------------------------------------------------------
+# getopt
+#----------------------------------------------------------------------
+def getopt(argv):
+    args = []
+    options = {}
+    if argv is None:
+        argv = sys.argv[1:]
+    index = 0
+    count = len(argv)
+    while index < count:
+        arg = argv[index]
+        if arg != '':
+            head = arg[:1]
+            if head != '-':
+                break
+            if arg == '-':
+                break
+            name = arg.lstrip('-')
+            key, _, val = name.partition('=')
+            options[key.strip()] = val.strip()
+        index += 1
+    while index < count:
+        args.append(argv[index])
+        index += 1
+    return options, args
+
+
+#----------------------------------------------------------------------
+# main
+#----------------------------------------------------------------------
+def main(argv):
+    if argv is None:
+        argv = sys.argv[1:]
+    options, args = getopt(argv)
+    return 0
+
+
+#----------------------------------------------------------------------
 # testing suit
 #----------------------------------------------------------------------
 if __name__ == '__main__':
@@ -108,7 +181,12 @@ if __name__ == '__main__':
         t = chatgpt_lazy(msgs)
         print(t)
         return 0
-    test1()
+    def test2():
+        text = execute(['dir', 'd:\\temp'])
+        print(text)
+        print(CallGit('status'))
+        return 0
+    test2()
 
 
 
