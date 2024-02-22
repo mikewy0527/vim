0761a0a8a784d728194159ab0f28695b386ee817
skywind3000 <skywind3000@163.com>
2024-02-23 00:14:31 +0800

    update dired

--- lua/plugins/options.lua
+++ lua/plugins/options.lua
@@ -103,6 +103,22 @@ return {
 				})
 		end,
 	},
+
+	{
+		"X3eRo0/dired.nvim",
+		enabled = package_enabled('dired'),
+		dependencies = {"MunifTanjim/nui.nvim"},
+		config = function()
+			require("dired").setup {
+				path_separator = "/",
+				show_banner = false,
+				show_icons = false,
+				show_hidden = true,
+				show_dot_dirs = true,
+				show_colors = true,
+			}
+		end
+	},
 }
 
 
